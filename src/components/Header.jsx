86ad51b7f065a4ede78f0771c5545158bcdf6fa1
horitomoyuki <eziltom0131@gmail.com>
2022-02-02 08:01:41 +0900

    プロフィール画面作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-export const Header = () => {
-  const componentName = () => 'Tom';
+import CoverImage from '../images/cover-image.png';
+import ProfileImage from '../images/profile-image.png';
 
+export const Header = () => {
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName()}です。
-    </div>
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            <h1 className="title-text">Horitomo</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
