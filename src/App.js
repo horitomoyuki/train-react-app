73c10289872978abc44a01a0484e690ba0c7785a
horitomoyuki <eziltom0131@gmail.com>
2022-02-02 21:48:12 +0900

    SkillとContactコンポーネント

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
   );
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,13 @@
-export const Header = () => {
-  const componentName = () => 'Header';
-
+export const Contact = () => {
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName()}です。
+    <div id="contact" className="contact">
+      <div className="container">
+        <div className="contact-contents">
+          <h2>Contact to Me!</h2>
+          <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
+          <a href="https://twitter.com/" className="btn">Contact Us</a>
+        </div>
+      </div>
     </div>
   );
 };
