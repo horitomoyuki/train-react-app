2bdde03c401067522fecb24e80397ca03a229bdc
horitomoyuki <eziltom0131@gmail.com>
2022-02-06 22:03:53 +0900

    取得データを整形

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,10 +2,32 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 export const Skills = () => {
-  const [data, setData] = useState(null);
-  console.log(data);
-  // useEffect(() => { axios.get('https://api.github.com/users/USER_NAME/repos').then((response) => console.log(response)) }, []);
-  useEffect(() => {axios.get('https://api.github.com/users/USER_NAME/repos').then((response) => setData(response)) }, []);
+  const [languageList, setLanguageList] = useState([]);
+  console.log(languageList);
+  
+  useEffect(() => {
+    axios.get('https://api.github.com/users/USER_NAME/repos')
+      .then((response) => {
+        // プログラミング言語のデータを取得
+        const languageList = response.data.map(res => res.language);
+        // generateLanguageCountObj()は引数にallLanguageListを受け取り、それを整形して返す
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        setLanguageList(countedLanguageList);
+      });
+  }, []);
+  // allLanguageListからfilterを使い、null以外を抽出して配列を生成。
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    // new Set()で重複値を取り除いたuniqueLanguageListという配列を生成
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  };
   return (
     <div id="skills">
       <div className="container">
