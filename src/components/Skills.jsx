c5ba211f7e3ce028bf956b05752d6eafb1066898
horitomoyuki <eziltom0131@gmail.com>
2022-03-03 07:38:30 +0900

    APIからデータを取得する状態について表示

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useReducer } from "react";
 import axios from 'axios';
 import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
-
+import { requestStates } from '../constants';
 
 export const Skills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
@@ -40,6 +40,16 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            state.requestState === requestStates.locading && (
+              <p className="description">取得中...</p>
+            )
+          }
+          {
+            state.requestState === requestStates.error && (
+              <p className="description">エラーが発生しました</p>
+            )
+          }
         </div>
       </div>
     </div>
